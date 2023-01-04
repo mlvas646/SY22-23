f286a37056202cd053406e1d045fe8145800cfbd
Miguel <mavas646@learner.dvusd.org>
2023-01-04 08:17:49 -0700

    Hello
    My first push

---
+++ VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim myVariable As Decimal
+        If TextBox.Text = "" Then
+            myVariable = 32
+        Else
+            Decimal.TryParse(TextBox.Text, myVariable)
+        End If
+        Dim myVariable2 As Decimal
+        TextBox.Text = myVariable
+        Decimal.TryParse(TextBox.Text, myVariable2)
+        TextBox.Text = myVariable2 / 2
+    End Sub
+End Class
